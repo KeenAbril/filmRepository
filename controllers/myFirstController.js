7bb52c8b4c337cd30a40199dfdacd2c4fe188f40
Kniro04 <91148088+KeenAbril@users.noreply.github.com>
2022-01-27 16:08:15 +0100

    Crate film

--- controllers/myFirstController.js
+++
@@ -1,5 +0,0 @@
-module.exports.helloWorld = (req, res) => {
-  const responseObj = {msg: 'Hello world!'};
-  const responseStatus = 200;
-  res.status(responseStatus).send(responseObj);
-}
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
-require('dotenv');
+require('dotenv').config();
+const cors = require('cors');
+const connect = require('./database/connect');
 
-const myFirstController = require('./controllers/myFirstController');
+connect.createConnection();
+
+app.use(cors());
+app.use(express.json()); // Parseja bodies de json (ajax)
+app.use(express.urlencoded({extended: true})); //Parseja bodies de form
 
 // req = request
 // res = response
-app.post('/', myFirstController.helloWorld);
 
-app.use('/api/v1/user', require('./routes/userRoutes'));
+app.use('/api/v1/films/', require('./routes/filmRoutes'));
+
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('My First API running!');
+  console.log(`My First API running on port: ${process.env.PORT}`);
 });
